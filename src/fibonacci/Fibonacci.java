ad858f54c535adebf83102dfaa81f71a4cc2cc71
Manohar <manohar.mangali@improve10x.com>
2023-07-12 12:04:13 +0530

    I wrote fibonacci series

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,21 @@
+package fibonacci;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        int num = fibonacci.find(8);
+       System.out.println(num);
+    }
+    public  int find(int n) {
+        if (n <= 0) {
+            return -1;
+        } else if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }else {
+            return find(n-1) + find(n-2);
+        }
+    }
+    }
+//    0,1,1,2,3,5,8,13,21
