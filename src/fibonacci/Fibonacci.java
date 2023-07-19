b9ed4f271c50f6a13c0b57d1e03da99c75cbe379
Manohar <manohar.mangali@improve10x.com>
2023-07-19 14:39:36 +0530

    I wrote test case for negative number returns negative One

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,10 @@
+package fibonacci;
+
+public class Fibonacci {
+    public int find(int number) {
+        if (number<0){
+            return -1;
+        }
+        return 0;
+    }
+}
---
+++ src/fibonacci/FibonacciTest.java
@@ -0,0 +1,20 @@
+package fibonacci;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FibonacciTest {
+    @Test
+    public void nothing(){
+
+    }
+
+    @Test
+    public void givenNegativeValue_returnNegativeOne(){
+        Fibonacci fibonacci = new Fibonacci();
+        int negativeFibonacci = fibonacci.find(-5);
+        assertEquals(-1,negativeFibonacci);
+    }
+}
