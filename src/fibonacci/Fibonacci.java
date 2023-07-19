eae0795c6bc711db2ba6f9a20ff1f417680d6f1e
Manohar <manohar.mangali@improve10x.com>
2023-07-19 15:24:11 +0530

    I refactored code and i decresed code

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -2,16 +2,12 @@ package fibonacci;
 
 public class Fibonacci {
     public int find(int number) {
-        if (number<0){
+        if (number <= 0) {
             return -1;
-        } else if (number == 0) {
-            return -1;
-        } else if (number == 1) {
-          return 0;
-        } else if (number == 2) {
-            return 1;
-        }else {
-           return find(number-1) + find(number-2);
+        } else if (number == 1 || number == 2) {
+            return number - 1;
+        } else {
+            return find(number - 1) + find(number - 2);
         }
     }
 }
