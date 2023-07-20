f4c9812b4fc678751facc8941424294b58748526
Manohar <manohar.mangali@improve10x.com>
2023-07-20 15:07:11 +0530

    In NameInverter I wrote code for name return name

--- src/test/com/improve10x/tdd/nameinverter/NameInverterTest.java
+++ src/test/com/improve10x/tdd/nameinverter/NameInverterTest.java
@@ -1,19 +1,30 @@
 package com.improve10x.tdd.nameinverter;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NameInverterTest {
+    private NameInverter nameInverter;
+    @BeforeEach
+    public void setup(){
+        nameInverter = new NameInverter();
+    }
     @Test
     public void nothing(){
 
     }
     @Test
     public void givenEmpty_returnEmpty(){
-        NameInverter nameInverter = new NameInverter();
         String invertedName = nameInverter.invertName("");
         assertEquals("",invertedName);
+    }
+
+    @Test
+    public void givenName_returnName(){
+        String invertedName = nameInverter.invertName("name");
+        assertEquals("name",invertedName);
     }
 }
