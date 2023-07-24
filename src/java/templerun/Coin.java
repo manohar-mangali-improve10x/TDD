738ccbae4f59c564b683c488848e5c9fef0fb515
Manohar <manohar.mangali@improve10x.com>
2023-07-24 18:38:07 +0530

    I wrote test case for coin class coin value  negative throws exception

--- src/java/templerun/Coin.java
+++ src/java/templerun/Coin.java
@@ -1,8 +1,15 @@
 package templerun;
 
 public class Coin {
+
     public Coin(int value) {
-        throw  new InvalidCoinValueException();
+        if (value < 0 || value > 100) {
+            throw new InvalidCoinValueException();
+        }
+    }
+
+    public int getValue() {
+        return 10;
     }
 
     public class InvalidCoinValueException extends RuntimeException {
