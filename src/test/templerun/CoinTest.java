edd411acd50eff64e72b3b90d17a6c07c61548b6
Manohar <manohar.mangali@improve10x.com>
2023-07-24 18:31:02 +0530

    I wrote test case for coin class coin value  negative throws exception

--- src/test/templerun/CoinTest.java
+++ src/test/templerun/CoinTest.java
@@ -15,4 +15,10 @@ public class CoinTest {
                 () -> new Coin(-1),
                 "Coin value should be between 0 to 100");
     }
+    @Test
+    public void given101_thenThrowsInvalidCoinException(){
+        assertThrows(Coin.InvalidCoinValueException.class,
+                () -> new Coin(-101),
+                "Coin value should be between 0 to 100");
+    }
 }
