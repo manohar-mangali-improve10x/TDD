cbf103f1b0af522b3bc4c7c83e15e63959de94c8
Manohar <manohar.mangali@improve10x.com>
2023-07-25 16:38:46 +0530

    I worte code for obstacle class and giveExplorer then returns Explorer

--- src/java/templerun/Obstacle.java
+++ src/java/templerun/Obstacle.java
@@ -6,6 +6,9 @@ public class Obstacle {
 
     public Obstacle(String name, int damage) {
         this.name = (name == null) ? "" : name;
+        if (damage < 0 || damage >100){
+            throw new InvalidDamageException();
+        }
     }
     public void collide(Character character){
         System.out.println(name + "has collide with" + character.getName() + "caused" + damage + "damage..");
@@ -14,5 +17,8 @@ public class Obstacle {
 
     public String getName() {
         return name;
+    }
+
+    public class InvalidDamageException extends RuntimeException{
     }
 }
--- src/test/templerun/ObstacleTest.java
+++ src/test/templerun/ObstacleTest.java
@@ -4,24 +4,40 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ObstacleTest {
     @Test
-    public void givenNameNull_returnEmpty(){
+    public void givenNameNull_thenGetNameCalled_returnEmpty(){
         Obstacle obstacle = new Obstacle(null,100);
         assertEquals("",obstacle.getName());
     }
 
     @Test
-    public void givenNameEmpty_returnEmpty(){
+    public void givenNameEmpty_thenGetNameCalled_returnEmpty(){
         Obstacle obstacle = new Obstacle("",100);
         assertEquals("",obstacle.getName());
     }
 
     @Test
-    public void givenNameExplorer_returnExplorer(){
+    public void givenNameExplorer_thenGetNameCalled_returnExplorer(){
         Obstacle obstacle = new Obstacle("Explorer",100);
         assertEquals("Explorer",obstacle.getName());
     }
+
+    @Test
+    public void givenDamageMinusOne_thenThrowsInvalidDamageException(){
+        assertThrows(Obstacle.InvalidDamageException.class,
+                () -> new Obstacle("name",-1),
+                "Health should be between 0 to 100");
+    }
+
+    @Test
+    public void givenDamage101_thenThrowsInvalidDamageException(){
+        assertThrows(Obstacle.InvalidDamageException.class,
+                () -> new Obstacle("name",101),
+                "Health should be between 0 to 100");
+    }
+
 
 }
